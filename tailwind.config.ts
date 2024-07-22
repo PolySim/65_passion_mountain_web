ef857866292ecc3c1a8c2bacc0d3f1a8da875665
Simon <simon.desdevises@gmail.com>
2024-07-22 15:52:52 +0200

    Initial commit from Create Next App

---
+++ tailwind.config.ts
@@ -0,0 +1,58 @@
+import type { Config } from "tailwindcss";
+
+const config = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        rubik: "'Rubik', sans-serif",
+        bebas: '"Bebas Neue", sans-serif',
+        m2: '"Helvetica, M PLUS 2", sans-serif',
+      },
+      colors: {
+        green: {
+          dark: "#2c6e49",
+          light: "#4c956c",
+        },
+        black: "#0f0f0f",
+        melon: "#ffc9b9",
+        orange: "#D68C45",
+        gray: "#7d7c83",
+        yellow: {
+          light: "#fefee3",
+        },
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
+export default config;
