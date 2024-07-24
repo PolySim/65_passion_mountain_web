245d25487497661e6b6e43d5d19c7b2c9fd9a527
Desdevises Simon <simon.desdevises@gmail.com>
2024-07-24 16:26:17 +0200

    feat : add list images

--- src/app/[categoryId]/state/[stateId]/[hikingId]/page.tsx
+++ src/app/[categoryId]/state/[stateId]/[hikingId]/page.tsx
@@ -1,5 +1,6 @@
 import { getHiking } from "@/utils/hiking/hikingAction";
 import HeaderHiking from "@/components/hikes/hearderHinkig";
+import Image from "next/image";
 
 export default async function HikingPage({
   params,
@@ -11,6 +12,21 @@ export default async function HikingPage({
     <main className="flex-1 bg-yellow-light py-16 px-8">
       <div className="w-full max-w-7xl rounded-2xl overflow-hidden mx-auto shadow-md bg-white">
         <HeaderHiking hiking={hiking} />
+        <div className="flex flex-col lg:flex-row w-full">
+          <div className="flex-1 "></div>
+          <div className="flex flex-col gap-4 lg:border-l lg:border-gray-300 w-full lg:w-4/12 p-4">
+            {(hiking.images || []).map((image) => (
+              <Image
+                key={image}
+                src={`${process.env.API_URL}/hiking/getImage/${image}`}
+                alt={`image ${hiking.title} ${image}`}
+                width={500}
+                height={281}
+                className="aspect-video w-full object-cover rounded-lg cursor-pointer"
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </main>
   );
