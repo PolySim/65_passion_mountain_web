ddb913a70ed200ceea3babffcfd95b17679c63f4
Desdevises Simon <simon.desdevises@gmail.com>
2024-08-09 18:04:24 +0200

    feat : api ip

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost:3210", "217.160.24.233:3210", "217.160.24.233"],
+    domains: [
+      "localhost:3210",
+      "127.0.0.1",
+      "127.0.0.1:3210",
+      "13.37.217.215:3210",
+      "13.37.217.215",
+    ],
     remotePatterns: [
       {
         protocol: "http",
