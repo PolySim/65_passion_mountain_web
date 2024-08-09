ef49114346a96d8b0d70d8f433615a70c94927a8
Desdevises Simon <simon.desdevises@gmail.com>
2024-08-09 18:20:12 +0200

    fix : style card

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,6 +1,6 @@
 const Card = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="shadow-md rounded-2xl overflow-hidden w-full h-full bg-white hover:scale-105 transition duration-300">
+    <div className="shadow-md rounded-2xl overflow-hidden w-full h-fit bg-white hover:scale-105 transition duration-300 text-center">
       {children}
     </div>
   );
